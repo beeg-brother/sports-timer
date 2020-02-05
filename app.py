5c52d1d62a41413390f0ce0e81967b72342339b6
Hersh Kumar <hershkumar13@gmail.com>
2020-02-05 18:50:53 -0500

    place the mpl stuff in the right layout

--- app.py
+++ app.py
@@ -10,27 +10,27 @@ window = QWidget()
 main_hbox = QHBoxLayout()
 timer_vbox = QVBoxLayout()
 
-layout.addWidget(QPushButton('Top'))
+main_hbox.addWidget(QPushButton('Top'))
 updatePlotButton = QPushButton('UpdatePlot')
-layout.addWidget(updatePlotButton)
-window.setLayout(layout)
+main_hbox.addWidget(updatePlotButton)
+window.setLayout(main_hbox)
 
 
 fig = Figure(figsize=(5,5))
 dynamic_canvas = FigureCanvas(fig)
-layout.addWidget(dynamic_canvas)
+main_hbox.addWidget(dynamic_canvas)
 
 laps = [4,2,3,6,23,4,5,1,3,44,5,3,2]
 
 axes = dynamic_canvas.figure.add_subplot(111)
-axes.plot(laps, 'r-')
+axes.plot(laps, 'r')
 axes.set_title("Lap Statistics")
 laps = [4,2,3,6,23,4,5,1,3,44,5,3,2,2,4,5,3,3,4,1,2,2,4,5,2,2,2,2,2,2,2,2,2,2,2,2,22]
 
 
 def updatePlot(axes,data):
 	axes.cla()
-	axes.plot(laps,'r-')
+	axes.plot(laps,'r')
 	fig.canvas.draw()
 	fig.canvas.flush_events()
 
