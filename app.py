d09393247adf02617ffa9f773490c88166ea03c5
Hersh Kumar <hershkumar13@gmail.com>
2020-02-07 12:38:56 -0500

    add documentation

--- app.py
+++ app.py
@@ -26,13 +26,15 @@ main_vbox.addWidget(timer_label)
 
 main_vbox.addStretch(.25)
 
+# define the area where the buttons are
 timer_button_hbox = QHBoxLayout()
+# create the button objects
 start_button = QPushButton('Start')
 pause_button = QPushButton('Pause')
 lap_button = QPushButton('Lap')
 reset_button = QPushButton('Reset')
 resume_button = QPushButton('Start')
-
+# add the two buttons that we start with, laps and starting the timer
 timer_button_hbox.addWidget(lap_button)
 timer_button_hbox.addWidget(start_button)
 
@@ -50,6 +52,7 @@ laps = []
 axes = dynamic_canvas.figure.add_subplot(111)
 axes.plot(laps, 'r')
 axes.set_title("Lap Statistics")
+# add the canvas to the gui
 stats_hbox.addWidget(dynamic_canvas)
 
 window.setLayout(main_vbox)
@@ -72,7 +75,7 @@ def truncate(num, decs):
 	return int(num*div) / div
 
 timer_lock = Lock()
-
+# defines the loop that increments the time
 def counter():
 	global current_time
 	while(True):
@@ -81,18 +84,20 @@ def counter():
 		start_time = time_ns()
 		sleep(.05)
 		current_time += time_ns() - start_time
+		# modify the label of the timer
 		timer_label.setText(str(time_format(seconds = truncate(current_time/1000000000, 2)))[:-4])
 
-
+# define the timer thread
 timer = Thread(name='timer', target = counter)
 
+# what to do when someone hits the start button
 def start_helper():
 	timer.start()
 	start_button.setParent(None)
 	timer_button_hbox.addWidget(pause_button)
 
 start_button.clicked.connect(start_helper)
-
+# what to do when someone hits the pause button
 def pause_helper():
 	timer_lock.acquire()
 	pause_button.setParent(None)
@@ -102,7 +107,7 @@ def pause_helper():
 	timer_button_hbox.addWidget(resume_button)
 		
 pause_button.clicked.connect(pause_helper)
-
+# what to do when someone hits the resume button
 def resume_helper():
 	timer_lock.release()
 	resume_button.setParent(None)
@@ -112,30 +117,39 @@ def resume_helper():
 
 resume_button.clicked.connect(resume_helper)
 
+# what to do when someone hits the reset button
 def reset_helper():
 	global current_time
 	global laps
 	global last_lap
 	current_time = 0
 	last_lap = 0
+	# get rid of the reset and resume buttons
 	reset_button.setParent(None)
 	resume_button.setParent(None)
+	# add the lap and resume buttons
 	timer_button_hbox.addWidget(lap_button)
 	timer_button_hbox.addWidget(resume_button)
+	# update the timer count
 	timer_label.setText(str(time_format(milliseconds = 0)))
+	# reset the lap data
 	laps = []
+	# update the chart
 	updatePlot(axes, laps)
 
 reset_button.clicked.connect(reset_helper)
-
+# what to do when someone hits the lap button
 def lap_helper():
 	global current_time
 	global last_lap
 	time_lap_button_clicked = current_time
 	delta_time = time_lap_button_clicked - last_lap
+	# check to make sure that the timer is actually running
 	if delta_time > 0:
+		# add to the lap data
 		laps.append((current_time - last_lap)/1000000000)
 		last_lap = current_time
+		# plot the new  stats
 		updatePlot(axes, laps)
 
 lap_button.clicked.connect(lap_helper)
